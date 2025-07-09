104f97efb1c39fdbc1f608b3d7a57f0b62d7521c
Akikoke <sherbets9999@yahoo.co.jp>
2025-07-09 12:47:12 -0700

    third version

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 import random
 
-# Prerequisites
 # Dictionary of words with difficulty levels, dictionary can be expanded later
 words = {
-    'Easy':     ['Lime', 'Salt'],
-    'Normal':   ['Avocado', 'Onion', 'Tomato', 'Lemon'],
-    'Hard':     ['Coriander', 'Garlic']
+    'Easy':     ['pepper', 'salt'],
+    'Normal':   ['rosemary', 'oregano'],
+    'Hard':     ['coriander', 'cinnamon']
 }
 
 # Difficulty level as a number
@@ -14,17 +13,17 @@ level_map = {'1': 'Easy', '2': 'Normal', '3': 'Hard'}
 # Tries allowed after incorrect guessed, tries allowed can be expanded later
 tries_by_level = {'Easy': 5, 'Normal': 4, 'Hard': 3}
 
-# Start
+# Game Start
 # Prompt the user to select a difficulty level using a number
-print('\033[95mWelcome to Hangman!\nGuess the word, Guacamole ingredients!\033[0m\n')
+print('Welcome to Hangman!\nGuess the word, Guacamole ingredients!')
 
 while True:
     level_selection = input(
         f"Select a difficulty level (1-3)\n"
-        "1. Easy\n"
-        "2. Normal\n"
-        "3. Hard\n"
-        ": "
+        '1. Easy\n'
+        '2. Normal\n'
+        '3. Hard\n'
+        ': '
     ).strip()
 
     if level_selection in level_map:
@@ -37,14 +36,14 @@ while True:
 # Select a random answer word from the dictionary
 word = random.choice(words[level]).lower()
 tries = tries_by_level[level]
-guessed_letters = ['_'] * len(word)
-incorrected_letters = []
+guessed = ['_'] * len(word)
+incorrect_letters = []
 
 # Prompt the user to input a letter
-while tries > 0 and '_' in guessed_letters:
-    print(f'\n\033[96mCurrent Word:\033[0m {" ".join(guessed_letters)}')
-    print(f'Incorrect guessed letters: {", ".join(incorrected_letters)}')
-    guess = input('\033[96mGuess a letter: \033[0m').lower()
+while tries > 0 and '_' in guessed:
+    print(f'Current Word: {" ".join(guessed)}')
+    print(f'Incorrect guessed letters: {", ".join(incorrect_letters)}')
+    guess = input('Guess a letter: ').lower()
     print(f'Incorrect guesses remaining: {tries}')
 
     # If the letter is invalid length and alphabet
@@ -53,7 +52,7 @@ while tries > 0 and '_' in guessed_letters:
         continue
 
     # If the letter is already guessed
-    if guess in guessed_letters or guess in incorrected_letters:
+    if guess in guessed or guess in incorrect_letters:
         print('The letter is already guessed.\n')
         continue
 
@@ -62,17 +61,17 @@ while tries > 0 and '_' in guessed_letters:
         for i, letter in enumerate(word):
             if letter == guess:
                 # Change the first letter to uppercase
-                guessed_letters[i] = guess.upper() if i == 0 else guess
+                guessed[i] = guess.upper() if i == 0 else guess
         print(f'Good job! "{guess}" is in the word.\n')
-    # If the letter is not found in the word, add it to the incorrect letters list
-    # and reduce tries
+    # If the letter is not found in the word, add it to the incorrect letters list.
+    # Reducing tries
     else:
-        incorrected_letters.append(guess)
+        incorrect_letters.append(guess)
         tries -= 1
         print(f'Sorry, "{guess}" is not in the word.\n')
 
 # End
-if '_' not in guessed_letters:
-    print(f'\033[95mCongratulations! You guessed the word: "{word.capitalize()}"\033[0m')
+if '_' not in guessed:
+    print(f'Congratulations! You guessed the word: "{word.capitalize()}"')
 else:
-    print(f'\033[91mGame over! The word was: "{word.capitalize()}"\033[0m')
+    print(f'Game over! The word was: "{word.capitalize()}"')
